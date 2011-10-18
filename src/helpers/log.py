891cd61b785aeef7d45fd1aa41ddf6bdd5346526
Benjamin Worwa <benjamin.worwa@ciens.ucv.ve>
2011-10-17 19:36:31 -0430

    improved logging helper

---
+++ src/helpers/log.py
@@ -0,0 +1,32 @@
+from datetime import date, datetime
+from os import path, makedirs
+
+current_path_to_log = "../logs/"
+
+def log_this(message, section):
+
+	"""Logs the current [message] in the directory /logs/[section]/[current_date].log. If [section] does not exist it is automatically created"""
+
+	global current_path_to_log
+
+	current_path_to_log = current_path_to_log + section + "/"
+
+	#TODO: try - except for makedirs
+
+	if not path.exists(current_path_to_log):
+
+		makedirs(current_path_to_log)
+
+	now = datetime.now()
+
+	current_path_to_log = (current_path_to_log + "/%d-%d-%d.log") % (now.day, now.month, now.year)
+
+	message = ("[%d:%d:%d] " + message) % (now.hour, now.minute, now.second)
+
+	log = open(current_path_to_log, "a")
+
+	log.write(message)
+
+	log.close()
+
+	del message, section, current_path_to_log, now, log
